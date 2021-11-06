0b29fbf9e0c6dd1390c7c2de56524835b627eb01
Kareem <kareem.shehada@gmail.com>
2021-11-06 14:05:06 -0700

    Co-authored-by: dyslexit <dyslexit@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
 require('dotenv').config();
 
 const { Client, Intents } = require('discord.js');
+
+// Intents: Guilds, VoiceStates, GuildMesssages
 const intents = [Intents.FLAGS.GUILDS, Intents.GUILD_VOICE_STATES, Intents.GUILD_MESSAGES];
 
+// Create Client
 const client = new Client({ intents });
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
+// Login
 client.login(process.env.token);
 
