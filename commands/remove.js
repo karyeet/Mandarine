ea7c53b74891c6e157e1d299667f9086adb2f553
Kareem <28524112+karyeet@users.noreply.github.com>
2021-12-09 14:22:07 -0800

    Don't allow removal of the 0th queue item

--- commands/remove.js
+++ commands/remove.js
@@ -12,18 +12,28 @@ function remove(message, args) {
 		message.react(reactions.negative);
 		return false;
 	}
-	// set index to remove to number(args)
-	const indexToRemove = Number(args);
+
 	// if args cannot be a number or does not exist, dont do anything
 	if (isNaN(Number(args)) || !args) {
 		message.react(reactions.confused);
 		return false;
 	}
+
+	// set index of item to remove to number(args)
+	const indexToRemove = Number(args);
+
+	// Don't allow removal of the 0th queue item, as this will remove the currently playing song which should be done using skip
+	if (indexToRemove == 0) {
+		message.react(reactions.negative);
+		return false;
+	}
+
 	// if there is no queue or there is no item at the specified index, dont do anything
 	if (!queue[message.guild.id] || !queue[message.guild.id][indexToRemove]) {
 		message.react(reactions.negative);
 		return false;
 	}
+
 	// remove item from array
 	queue[message.guild.id].splice(indexToRemove, 1);
 	message.react(reactions.positive);
