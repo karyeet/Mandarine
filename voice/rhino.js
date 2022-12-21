b1db38c252e052bc3c3a78b7f81c0ff8ab4b5230
Kareem <28524112+karyeet@users.noreply.github.com>
2022-12-21 00:42:45 -0800

    basic intent processor implementation

---
+++ voice/rhino.js
@@ -0,0 +1,48 @@
+const { Rhino } = require("@picovoice/rhino-node");
+
+const path = require("path");
+
+const { picoAccessKey } = require("../config.json");
+const accessKey = picoAccessKey;
+
+const contextPath = path.join(__dirname, "musicbot_en_windows_v2_1_0.rhn");
+
+// const rhino = new Rhino(accessKey, contextPath);
+
+/*
+intentDetectors = {channelid : rhino}
+*/
+// const intentDetectors = {};
+
+function processRhinoVoiceData(PCMprovider) {
+	return new Promise((resolve) => {
+		const rhino = new Rhino(accessKey, contextPath, 0.2, undefined, false);
+		let isFinalized;
+		PCMprovider.on("frame", function feedFrame(frame) {
+			// if rhino already finalized dont run
+			if (isFinalized) {
+				return;
+			}
+			// feed frames until rhino makes it decision
+			isFinalized = rhino.process(frame);
+			// console.log("isfinalized " + isFinalized);
+			if (isFinalized) {
+				// rhino has decided, stop feeding
+				PCMprovider.removeListener("frame", feedFrame);
+				const inference = rhino.getInference();
+				if (inference.isUnderstood) {
+					const intent = inference.intent;
+					// console.log("internal: " + intent);
+					rhino.release();
+					resolve(intent);
+				}
+				else {
+					rhino.release();
+					resolve(false);
+				}
+			}
+		});
+	});
+}
+
+module.exports = { processRhinoVoiceData };
