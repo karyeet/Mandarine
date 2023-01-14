179532684b81d88fc64f0548a4eb4bc1e193f69f
karyeet <28524112+karyeet@users.noreply.github.com>
2023-01-13 20:23:04 -0800

    fix embedded image not showing

--- commands/play.js
+++ commands/play.js
@@ -91,10 +91,10 @@ function addDZdataToQueue(message, data) {
 	queue[message.guild.id].push(queueData);
 	message.react(reactions.positive);
 	// send embed and delete after 60 seconds
-	if (data.metadata.image) {
+	if (data.metadata.common.picture && data.metadata.common.picture[0]) {
 		message.reply(
 			{ embeds:[songAdded(queueData, queue[message.guild.id].length - 1)],
-				files:[{ "name":"thumb.jpg", "attachment":data.metadata.common.picture ? data.metadata.common.picture[0].data : undefined }],
+				files:[{ "name":"thumb.jpg", "attachment":data.metadata.common.picture[0].data}],
 			})
 			.then(msg => {
 				setTimeout(() => msg.delete(), 60000);
